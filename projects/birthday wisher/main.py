8406f1e54deee5a51017db4d3dcdd8e8d6983833
Jana-varshan <167455585+Jana-varshan@users.noreply.github.com>
2024-04-19 22:12:46 +0530

    Update main.py

--- projects/birthday wisher/main.py
+++ projects/birthday wisher/main.py
@@ -5,43 +5,6 @@ import random
 from tkinter import *
 from tkinter import messagebox
 
-# ans=input("do you want to add data ?")
-# # ui setup
-# while ans=="yes":
-#     window = Tk()
-#     window.title("birthday wisher")
-#     window.config(pady=20,padx=20,background="light blue")
-#
-#     # canvas= Canvas(height=300,width=300)
-#     # canvas.grid(column=1,row=1)
-#     label_n=Label(text="Name:",font=("arial",18,"italic bold"),background="light blue")
-#     label_n.grid(column=1,row=2)
-#     label_n.config(pady=10,padx=10)
-#
-#     label_m=Label(text="Mail id:",font=("arial",18,"italic bold"),background="light blue")
-#     label_m.grid(column=1,row=3)
-#     label_m.config(pady=10,padx=10)
-#
-#     label_d=Label(text="DOB:",font=("arial",18,"italic bold"),background="light blue")
-#     label_d.grid(column=1,row=4)
-#     label_d.config(pady=10,padx=10)
-#
-#     name=Entry(width=36)
-#     name.grid(column=2,row=2,columnspan=2)
-#
-#     mail=Entry(width=36)
-#     mail.grid(column=2,row=3,columnspan=2)
-#
-#     mail=Entry(width=20)
-#     mail.grid(column=2,row=4)
-#
-#     lab_h=Label(text="Data base entry",font=("arial",24,"italic bold"))
-#     lab_h.grid(column=1,row=1,columnspan=2)
-#
-#     window.mainloop()
-#mind
-
-
 mail = "angelayutest@gmail.com"
 password = "tzxpzsamqtoxzjys"
 
